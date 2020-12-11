2779a00a6cda53c308aa5c474629ebf67ba5ca40
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-12-11 15:54:21 +0530

    Added SEO,PWA & Github Action

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+let config = {
+    title:"HPFanfiction Recommender",
+    description:"Recommendation from r/HPFanfiction",
+    url:"https://hpffrec.haceksta.org",
+}
+export default {
+    title:config.title,
+    description:config.description,
+    canonical:config.url,
+    openGraph: {
+      url:  config.url,
+      title: config.title,
+      description: config.description,
+      type: 'website',
+      url: config.url,
+      site_name: config.title,
+      images: [
+        {
+          url: config.url+'/og.png',
+          width: 3750,
+          height: 1969,
+          alt: 'OG Image'
+      },
+      ]
+    },
+  };
---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
+
+module.exports = withPlugins([
+  [
+    withPWA,
+    {
+      pwa: {
+        dest: "public",
+      },
+    },
+  ],
+]);
