6f572b1246c48c0c03583372a6185e73c77a7037
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-12-18 16:30:55 +0530

    Moved to GoatCounter

--- pages/_document.js
+++ pages/_document.js
@@ -7,25 +7,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-            crossOrigin={true}
-          />
+        <script data-goatcounter="https://hpffrec.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>          
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" crossOrigin={true}></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
         </Head>
         <body>
           <Main />
